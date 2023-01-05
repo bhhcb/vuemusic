cc47cc6cc592b3718f8b0b73915d8db9be467878
bhhcb <114869118+bhhcb@users.noreply.github.com>
2023-01-05 19:00:16 +0800

    Update aboutUs.vue

--- src/views/aboutUs.vue
+++ src/views/aboutUs.vue
@@ -1,19 +1,17 @@
 <template>
   <div><h1 style="font-size: 500%;text-align: center;">创作者团队</h1></div>
-  <div style="display: flex;margin-left: 28%;"><h2>21开发王雨竺</h2><h2 style="margin-left:10%">21开发王明月</h2><h2 style="margin-left:10%">21开发汉天雨</h2></div>
+  <div style="display: flex;margin-left: 28%;"></div>
   <el-button style="margin-left:42%;font-size: 200%;" type="primary" text @click="dialogVisible = true">
     查看详细信息
   </el-button>
 
   <el-dialog
     v-model="dialogVisible"
-    title="成员贡献度"
+    title="暂无内容"
     width="30%"
     :before-close="handleClose"
   >
-    <span>21开发王雨竺：40%</span><br>
-    <span>21开发王明月：30%</span><br>
-    <span>21开发汉天雨：30%</span><br>
+
     <template #footer>
       <span class="dialog-footer">
         <el-button @click="dialogVisible = false">取消</el-button>
